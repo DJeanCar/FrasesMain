766c6a0f582e85a41f2c9e2016542697ff0034af
DJeanCar <jean_carlos104@hotmail.com>
2017-08-04 23:39:33 -0500

    author

--- frasesmain/apps/main/admin.py
+++ frasesmain/apps/main/admin.py
@@ -1,13 +1,19 @@
 from django.contrib import admin
 
-from .models import Category, Phrase
+from .models import Category, Phrase, Author
 
 @admin.register(Category)
 class CategoryAdmin(admin.ModelAdmin):
 	
 	list_display = ('name', )
 
+@admin.register(Author)
+class AuthorAdmin(admin.ModelAdmin):
+	
+	list_display = ('name', )
+
 @admin.register(Phrase)
 class PhraseAdmin(admin.ModelAdmin):
 	
-	list_display = ('phrase', 'category', )
+	list_display = ('phrase', 'category', 'author' )
+	list_editable = ('author', )
--- frasesmain/apps/main/models.py
+++ frasesmain/apps/main/models.py
@@ -17,10 +17,25 @@ class Category(models.Model):
 		self.slug = slugify(self.name)
 		super(Category, self).save()
 
+class Author(models.Model):
+
+	name = models.CharField(max_length=100)
+	slug = models.SlugField(unique=True, editable=False, max_length=70)
+
+	description = models.TextField(max_length=1000)
+
+	def __str__(self):
+		return self.name
+
+	def save(self):
+		self.slug = slugify(self.name)
+		super(Author, self).save()
+
 
 class Phrase(models.Model):
 
 	category = models.ForeignKey(Category)
+	author = models.ForeignKey(Author, null=True, blank=True)
 	phrase = models.TextField(max_length=1000)
 
 	created_at = models.DateTimeField(auto_now_add=True)
